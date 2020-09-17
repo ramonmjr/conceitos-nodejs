2d6eb703209502546050762c9128f10fec715958
Ramon Marques <ramon.marques@enova.educacao.ba.gov.br>
2020-09-17 15:03:54 -0300

    desafio feito

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid } = require('uuid');
+const { v4: uuid } = require("uuid");
 
 const app = express();
 
@@ -10,24 +10,67 @@ app.use(cors());
 
 const repositories = [];
 
+function validateResource(request, response, next) {
+  const { id } = request.params;
+
+  repoIndex = repositories.findIndex((repository) => repository.id === id);
+
+  if (repoIndex < 0) {
+    return response.status(400).json("bad request!");
+  }
+
+  return next();
+}
+
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.status(200).json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs, likes = 0 } = request.body;
+  const repo = { id: uuid(), title, url, techs, likes };
+
+  repositories.push(repo);
+
+  return response.status(201).json(repo);
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.put("/repositories/:id", validateResource, (request, response) => {
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+  const repoIndex = repositories.findIndex(
+    (repository) => repository.id === id
+  );
+
+  let repo = repositories[repoIndex];
+  repo = { id, title, url, techs, likes: repo.likes };
+
+  repositories[repoIndex] = repo;
+
+  return response.status(200).json(repositories[repoIndex]);
 });
 
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
+app.delete("/repositories/:id", validateResource, (request, response) => {
+  const { id } = request.params;
+
+  const repoIndex = repositories.findIndex(
+    (repository) => repository.id === id
+  );
+
+  repositories.splice(repoIndex, 1);
+
+  return response.status(204).send();
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+app.post("/repositories/:id/like", validateResource, (request, response) => {
+  const { id } = request.params;
+  const repoIndex = repositories.findIndex(
+    (repository) => repository.id === id
+  );
+  const repo = repositories[repoIndex];
+  repo.likes += 1;
+  repositories[repoIndex] = repo;
+  return response.status(200).json(repositories[repoIndex]);
 });
 
 module.exports = app;
